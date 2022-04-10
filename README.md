47e0517e4ccd47ab1d8e941d4d79f0592378df04
David Howe <90102914+DavidHoweOTU@users.noreply.github.com>
2022-04-09 22:48:47 -0400

    Formatted first scene generation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Chatbot
+# Pokémon Search Bot
  Final Project for CSCI-2020U
+
+Resources:
+<br>https://docs.oracle.com/javafx/2/ui_controls/choice-box.htm
--- src/main/java/com/example/chatbot/ClientApplication.java
+++ src/main/java/com/example/chatbot/ClientApplication.java
@@ -1,8 +1,15 @@
 package com.example.chatbot;
 
 import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,16 +18,96 @@ import java.io.IOException;
 
 public class ClientApplication extends Application {
 
+    private final int windowHeight = 600;
+    private final int windowWidth  = 600;
+
+    private final ObservableList<String> types = FXCollections.observableArrayList(
+            "Any Type",
+            "Normal",
+            "Fire",
+            "Water",
+            "Electric",
+            "Grass",
+            "Ice",
+            "Fighting",
+            "Poison",
+            "Ground",
+            "Flying",
+            "Psychic",
+            "Bug",
+            "Rock",
+            "Ghost",
+            "Dragon",
+            "Dark",
+            "Steel",
+            "Fairy"
+    );
+
+
+
     public Scene generateSearchScene() {
         HBox root = new HBox();
-        root.getChildren().add(new ListView<String>());
 
-        return new Scene(new HBox());
+        ListView<String> listOfPokemon = new ListView<>();
+        listOfPokemon.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                // TODO - Switch to new scene when ListItem clicked
+                // listView.getSelectionModel().getSelectedItem()
+            }
+        });
+
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(10));
+        vBox.setSpacing(10);
+
+        TextField pokemonName = new TextField();
+        pokemonName.setPromptText("Pokémon Name");
+
+        ChoiceBox<String> type1 = new ChoiceBox<>(types);
+        ChoiceBox<String> type2 = new ChoiceBox<>(types);
+        ChoiceBox<String> generation = new ChoiceBox<>(
+                FXCollections.observableArrayList("Any Generation", "1", "2", "3", "4", "5", "6")
+        );
+        type1.setTooltip(new Tooltip("First type filter"));
+        type2.setTooltip(new Tooltip("Second type filter"));
+        generation.setTooltip(new Tooltip("Generation filter"));
+
+        CheckBox legendsOnly = new CheckBox("Legendaries Only");
+
+        Button searchButton = new Button("Search");
+        searchButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                // TODO - Update ListView with filtered entries
+            }
+        });
+
+        Button exitButton = new Button("Exit");
+        exitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                // TODO - Safely close client socket and end program
+            }
+        });
+
+        // Arrange items in scene
+        root.getChildren().add(listOfPokemon);
+        root.getChildren().add(vBox);
+        vBox.getChildren().add(pokemonName);
+        vBox.getChildren().add(type1);
+        vBox.getChildren().add(type2);
+        vBox.getChildren().add(generation);
+        vBox.getChildren().add(legendsOnly);
+        vBox.getChildren().add(searchButton);
+        vBox.getChildren().add(exitButton);
+
+        return new Scene(root, windowWidth, windowHeight);
     }
 
     public Scene generateDataScene() {
-
-        return new Scene(new VBox(), 600, 600);
+        // TODO - Format data scene
+        return new Scene(new VBox(), windowWidth, windowHeight);
     }
 
 
