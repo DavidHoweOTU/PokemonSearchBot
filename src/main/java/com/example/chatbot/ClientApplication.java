287bc8dd2ebe3c1b48dcfe185ff3e7027862baea
David Howe <90102914+DavidHoweOTU@users.noreply.github.com>
2022-04-14 17:09:41 -0400

    Implement Data UI

--- src/main/java/com/example/chatbot/ClientApplication.java
+++ src/main/java/com/example/chatbot/ClientApplication.java
@@ -8,8 +8,10 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -40,6 +42,8 @@ public class ClientApplication extends Application {
             "Steel",
             "Fairy"
     );
+
+    public final Pokemon testPokemon = new Pokemon("Charizard", "Fire", "Flying", "1", "False", "78", "84", "78", "109", "85", "100", "534");
 
 
 
@@ -103,20 +107,84 @@ public class ClientApplication extends Application {
         return new Scene(root, windowWidth, windowHeight);
     }
 
-    public Scene generateDataScene() {
+    public Scene generateDataScene(Pokemon p) {
         // TODO - Format data scene
-        VBox vBox = new VBox();
-        vBox.setPadding(new Insets(10));
+        VBox root = new VBox();
+        root.setPadding(new Insets(10));
 
-        TextField pokemonName = new TextField();
+        Label pName = new Label(p.name());
+        pName.setFont(Font.font(22));
 
-        return new Scene(vBox, windowWidth, windowHeight);
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        Label type = new Label("Type:");
+        Label pType1 = new Label(p.type1());
+        Label pType2 = new Label(p.type2());
+        Label gen = new Label("Generation:");
+        Label pGen = new Label(p.generation());
+        Label legend = new Label("Legendary:");
+        Label pLegend = new Label(p.isLegendary());
+
+        GridPane gridPane = new GridPane();
+        Label hp = new Label("HP:");
+        Label pHp = new Label(p.hp());
+        Label atk = new Label("Atk:");
+        Label pAtk = new Label(p.atk());
+        Label def = new Label("Def:");
+        Label pDef = new Label(p.def());
+        Label spa = new Label("SpA:");
+        Label pSpa = new Label(p.spa());
+        Label spd = new Label("SpD:");
+        Label pSpd = new Label(p.spd());
+        Label spe = new Label("Spe:");
+        Label pSpe = new Label(p.spe());
+        Label total = new Label("Total:");
+        Label pTotal = new Label(p.total());
+        Button backButton = new Button("Back");
+        backButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                // TODO - Return to the first scene
+            }
+        });
+
+        // add items to gridPane
+        gridPane.add(hp,    0, 0);
+        gridPane.add(atk,   0, 1);
+        gridPane.add(def,   0, 2);
+        gridPane.add(spa,   0, 3);
+        gridPane.add(spd,   0, 4);
+        gridPane.add(total, 0, 5);
+
+        gridPane.add(backButton, 0, 6);
+
+        gridPane.add(pHp,    1, 0);
+        gridPane.add(pAtk,   1, 1);
+        gridPane.add(pDef,   1, 2);
+        gridPane.add(pSpa,   1, 3);
+        gridPane.add(pSpd,   1, 4);
+        gridPane.add(pTotal, 1, 5);
+
+        // add items to root
+        root.getChildren().add(pName);
+        root.getChildren().add(hBox);
+        hBox.getChildren().add(type);
+        hBox.getChildren().add(pType1);
+        hBox.getChildren().add(pType2);
+        hBox.getChildren().add(gen);
+        hBox.getChildren().add(pGen);
+        hBox.getChildren().add(legend);
+        hBox.getChildren().add(pLegend);
+        root.getChildren().add(gridPane);
+
+        return new Scene(root, windowWidth, windowHeight);
     }
 
 
     @Override
     public void start(Stage stage) throws IOException {
         Scene scene = generateSearchScene();
+//        Scene scene = generateDataScene(testPokemon);
         stage.setTitle("Pokémon Search Bot");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/chatbot/ServerApplication.java
+++ src/main/java/com/example/chatbot/ServerApplication.java
@@ -3,6 +3,6 @@ package com.example.chatbot;
 public class ServerApplication {
 
     public static void main(String[] args) {
-
+        // TODO - Start server
     }
 }
